a212748b5da3b2d1a2e1f02363fb61c2e2e70194
Millie Yang <my2440@columbia.edu>
2016-06-16 21:39:07 -0400

    Updated files

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,9 +11,56 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
 	final static WikiFetcher wf = new WikiFetcher();
-	
+
+	private static String findFirstValidURL(Iterable<Node> iter, String currURL) {
+		for (Node node: iter) {
+			if (node instanceof Element) {
+				Element tempElement = (Element) node;
+				if (checkValid(currURL, tempElement)) {
+					return "http://en.wikipedia.org" + node.attr("href");
+				}
+			}
+        }
+        return null;
+	}
+
+	private static boolean checkValid (String currURL, Element element) {
+		String validLink = "http://en.wikipedia.org" + element.attr("href");
+		//base case
+		if (element.attr("href").equals("")) {
+			return false;
+		}
+		else if (currURL.equals(validLink)) {
+			return false;
+		}
+		else if (isItalics(element)) {
+			return false;
+		}
+		else if(element.attr("href").startsWith("/wiki/Help:")) {
+			return false;
+		}
+		else if (element.attr("href").startsWith("#cite_note")) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+
+	private static boolean isItalics(Element element) {
+		Element curr = element;
+		while (curr!= null) {
+			if (curr.tagName().equals("i") || curr.tagName().equals("em")) {
+				return true;
+			}
+			curr = element.parent();
+		}
+		return false;
+	}
+
+
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
 	 * 
@@ -28,24 +75,54 @@ public class WikiPhilosophy {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
+		System.out.println("testing");
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		boolean isPhilosophy = false;
+		List<String> urlList = new ArrayList<String>();
+		while (!isPhilosophy) {
+			System.out.println("ENTERED INTO WHILE LOOP");
+			Elements paragraphs = wf.fetchWikipedia(url);
+			Element firstPara = paragraphs.get(0);
+			System.out.println("the first para:" + firstPara);
+			urlList.add(url);
+			Iterable<Node> iter = new WikiNodeIterable(firstPara);
+			String firstValidURL = findFirstValidURL(iter, url);
+			System.out.println("firstValidURL" + firstValidURL);
+			String philosophy = "https://en.wikipedia.org/wiki/Philosophy";
+			if (firstValidURL!=null && url.equals(philosophy)) {
+				urlList.add(firstValidURL);
+				isPhilosophy = true;
+			}
+			if (firstValidURL!=null && (!url.equals(philosophy))) {
+				url = firstValidURL;
+			}
+			if (firstValidURL==null) {
+				System.out.println("An occur has occured.");
+				isPhilosophy = true;
+			}
+		}
+
+		for(String temporaryURL: urlList) {
+				System.out.println(temporaryURL);
+		}
+	}
         // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		// String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		// Elements paragraphs = wf.fetchWikipedia(url);
 
-		Element firstPara = paragraphs.get(0);
+		// Element firstPara = paragraphs.get(0);
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		// Iterable<Node> iter = new WikiNodeIterable(firstPara);
+		// for (Node node: iter) {
+		// 	if (node instanceof TextNode) {
+		// 		System.out.print(node);
+		// 	}
+  //       }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
+        // // the following throws an exception so the test fails
+        // // until you update the code
+        // String msg = "Complete this lab by adding your code and removing this statement.";
+        // throw new UnsupportedOperationException(msg);
+	
 }
